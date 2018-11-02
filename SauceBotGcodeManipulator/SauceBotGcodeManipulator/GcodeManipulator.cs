c3c375f7e276ee52c4c6e0494aac016ac84f8f1f
Adric <adric.landman@gmail.com>
2018-11-02 23:45:30 +1100

    Adding Gcode Manipulator

---
+++ SauceBotGcodeManipulator/SauceBotGcodeManipulator/GcodeManipulator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace SauceBotGcodeManipulator
+{
+    public class GcodeManipulator
+    {
+        private string fileName;
+        private bool relative;
+        private string prefix;
+        private int movementSize;
+        private string gcodeRegex = @"^(G0 |G1 |G2 |G3 )";
+        public GcodeManipulator(string fileName, bool relative, string prefix, int movementSize)
+        {
+            this.fileName = fileName;
+            this.relative = relative;
+            this.prefix = prefix;
+            this.movementSize = movementSize;
+
+            if (!File.Exists(fileName))
+                throw new Exception("File does not exist");
+        }
+
+        public void Run()
+        {
+            var newLines = new List<string>();
+            var regex = new Regex (gcodeRegex);
+            var extruderPos = movementSize;
+
+            var lines = File.ReadAllLines(fileName);
+            foreach(var line in lines){
+
+                if (regex.IsMatch(line)){
+                    newLines.Add($"{line} {prefix}{extruderPos}");
+                    if (relative)
+                        extruderPos += movementSize;
+                }
+                else 
+                    newLines.Add(line);
+            }
+
+            File.WriteAllText($"{fileName}_new", string.Join("\n", newLines));
+
+
+        }
+    }
+}
---
+++ SauceBotGcodeManipulator/SauceBotGcodeManipulator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SauceBotGcodeManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 4)
+                throw new Exception("Required [GCode Filename], [relative true/false], [Prefix E,Z], [Movement Size]");
+
+            var fileName = args[0];
+            if (!bool.TryParse(args[1], out bool relative))
+                throw new Exception("Required [GCode Filename], [relative true/false], [Prefix E,Z], [Movement Size]");
+
+            var prefix = args[2];
+
+            if (!Int32.TryParse(args[3], out int movementSize))
+                throw new Exception("Required [GCode Filename], [relative true/false], [Prefix E,Z], [Movement Size]");
+
+            var manipulator = new GcodeManipulator(fileName, relative, prefix, movementSize);
+            manipulator.Run();
+        }
+    }
+}
